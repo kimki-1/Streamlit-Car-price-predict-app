b393d839f44c9e788c49bc3efd4302b01e75ff28
kimki-1 <78473073+kimki-1@users.noreply.github.com>
2021-03-11 15:32:59 +0900

    version 3.0

--- eda_app.py
+++ eda_app.py
@@ -3,6 +3,10 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd 
 import tensorflow.keras
+import matplotlib.pyplot as plt 
+import matplotlib
+import seaborn as sns
+matplotlib.use('Agg')
 
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
@@ -23,6 +27,53 @@ def run_eda_app() :
 
     elif selected_radio == '통계치' :
         st.dataframe(car_df.describe())
+    
+    columns = car_df.columns
+    columns = list(columns)
+    
+    selected_columns = st.multiselect('컬럼을 선택하시오', columns)
+    
+    if len(selected_columns) != 0 :
+        st.dataframe(car_df[selected_columns])
+    else :
+        st.write('선택한 컬럼이 없습니다.')
+    
+    # 상관계수를 화면에 보여주도록 만듭니다. 
+    # 멀티셀렉트에 컬럼명을 보여주고,
+    # 해당 컬럼들에 대한 상관 계수를 보여주세요.
+    # 단, 컬럼들은 숫자 컬럼들만 멀티셀렉트에 나타나야합니다.
+    
+    
+    corr_columns = car_df.columns[ car_df.dtypes != object ] # car_df.corr().columns.values
+    selected_corr = st.multiselect('상관계수 컬럼을 선택', corr_columns)
 
+    if len(selected_corr) != 0 :
+        st.dataframe(car_df[selected_corr].corr())
+        # 위에서 선택한 컬럼들을 이용해서, 시본의 페어플롯을 그린다
+        fig = sns.pairplot(data = car_df[selected_corr])
+        st.pyplot(fig)
 
+    else :
+        st.write('선택한 컬럼이 없습니다.')
+
+    # 컬럼을 하나만 선택하면, 해당 컬럼의 max,min에 해당하는
+    # 사람의 데이터를 화면에 보여주는 기능
+    float_columns = car_df.columns[ car_df.dtypes == float ] 
+    selected_col = st.selectbox('컬럼 선택', float_columns )
+
+    min_data = car_df.loc[car_df[selected_col].min() == car_df[selected_col],]
+    st.markdown('### Min Data')
+    st.dataframe(min_data)
+
+    max_data = car_df.loc[car_df[selected_col].max() == car_df[selected_col],]
+    st.markdown('### Max Data')
+    st.dataframe(max_data)
+
+    # 고객이름을 검색 할 수 있는 기능 
+    fname = st.text_input('이름을 검색하세요')
+    find_customer = car_df['Customer Name'].str.contains(fname, case =False)
+    if len(fname) != 0  :
+        st.dataframe(car_df.loc[find_customer, ] )
+    else :
+        st.write('이름을 입력하세요')
 
--- ml_app.py
+++ ml_app.py
@@ -13,27 +13,38 @@ import joblib
 def run_ml_app():
     st.subheader('Machine Learning')
 
-    model = tf.keras.models.load_model('data/car_ai.h5')
-    sc_X = joblib.load('data/sc_X.pkl')
-    sc_y = joblib.load('data/sc_y.pkl')
-
+    # 1. 입력받기
     gender = st.radio('성별 선택 ', ['여자', '남자'])
     if gender == '여자' :
         gender = 0 
     elif gender == '남자' :
         gender = 1
 
-    age = st.number_input('나이', 1, 120)
-    salary = st.number_input('연봉($)',0)
-    card = st.number_input('카드 빛($)',0)
-    worth = st.number_input('순자산($)',0)
+    age = st.number_input('나이', 0, 120)
+    salary = st.number_input('연봉($)', 0)
+    card = st.number_input('카드 빛($)', 0)
+    worth = st.number_input('순자산($)', 0)
     
+    # 2. 예측하기 
+    # 2-1. 모델 불러오기
+    model = tf.keras.models.load_model('data/car_ai.h5')
+
+    # 2-2. 넘파이 어레이 만들기
     new_data = np.array( [ gender, age, salary, card, worth ])
+
+    # 2-3. 피처 스케일링 
     new_data = new_data.reshape(1, -1)
+    sc_X = joblib.load('data/sc_X.pkl')
     new_data_scaled = sc_X.transform(new_data)
 
+    # 예측 결과는, 스케일링 된 결과이므로, 다시 돌려놔야 한다
+    # 2-4. 예측한다 
     y_pred = model.predict(new_data_scaled)
+    sc_y = joblib.load('data/sc_y.pkl')
     y_pred_orginal = sc_y.inverse_transform(y_pred)
 
+    # 3. 결과를 화면에 보여준다
     if st.button('예측') :
-        st.title('당신의 자동차 가격은 {}$ 입니다'.format(y_pred_orginal[0][0]))
+        st.markdown('## 예측 결과 입니다.')
+        st.markdown('### 당신은 {:,.1f}$ 금액의 차를 구매 할 수 있습니다.'.format(y_pred_orginal[0][0]))
+    
